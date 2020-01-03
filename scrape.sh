5fe9e08a57ff825f32c6b38a04da2ddeb04e1ee9
Liam Best <liam@liambest.io>
2020-01-04 01:36:26 +1100

    Update scrape.sh
    Victoria data is gzip compressed - amended curl command.

--- scrape.sh
+++ scrape.sh
@@ -8,6 +8,6 @@ curl http://www.rfs.nsw.gov.au/feeds/majorIncidents.json > "/var/www/firedata/ns
 ln -sf "/var/www/firedata/nsw/${timestamp}.json" /var/www/firedata/nsw/latest.json
 
 ## get vic data
-curl http://emergency.vic.gov.au/public/osom-geojson.json > "/var/www/firedata/vic/${timestamp}.json"
+curl --compressed 'http://emergency.vic.gov.au/public/osom-geojson.json' > "/var/www/firedata/vic/${timestamp}.json"
 # symlink with most recent
 ln -sf "/var/www/firedata/vic/${timestamp}.json" /var/www/firedata/vic/latest.json
